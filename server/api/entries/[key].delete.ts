82e16ae8e6fa2e8c7307017a8f6615b4d840dbbe
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-01-29 15:51:11 +0100

    chore: requireSession first

--- server/api/entries/[key].delete.ts
+++ server/api/entries/[key].delete.ts
@@ -1,10 +1,10 @@
 import { useValidatedParams, z } from 'h3-zod'
 
 export default eventHandler(async (event) => {
+  const session = await requireUserSession(event)
   const { key } = await useValidatedParams(event, {
     key: z.string().min(1).max(100)
   })
-  const session = await requireUserSession(event)
 
   // Delete entry for the current user
   const storage = await useKV(String(session.user!.id))
--- server/api/todos/index.post.ts
+++ server/api/todos/index.post.ts
@@ -1,10 +1,10 @@
 import { useValidatedBody, z } from 'h3-zod'
 
 export default eventHandler(async (event) => {
+  const session = await requireUserSession(event)
   const { title } = await useValidatedBody(event, {
     title: z.string().min(1).max(100)
   })
-  const session = await requireUserSession(event)
 
   // List todos for the current user
   const todo = await useDatabase().insert(tables.todos).values({
