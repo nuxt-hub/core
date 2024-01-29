fc827b27a92f39392e5acdd6434058fd8557bb9f
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-01-29 16:00:49 +0100

    chore(storage): put & get file

--- server/api/storage/index.get.ts
+++ server/api/storage/index.get.ts
@@ -1,17 +1,8 @@
 export default eventHandler(async (event) => {
-  const session = await requireUserSession(event)
+  await requireUserSession(event)
 
   // List files for the current user
 
-  const res = await useBucket().list()
-
-  return res.objects
-
-  // const keys = await storage.getKeys()
-  // // const items = await storage.getItems(keys)
-  // const items = await Promise.all(keys.map(async (key) => {
-  //   const value = await storage.getItem(key)
-  //   return { path: key, content: value }
-  // }))
-  // return []
+  const bucket = useBucket()
+  return serveFiles(bucket)
 })
