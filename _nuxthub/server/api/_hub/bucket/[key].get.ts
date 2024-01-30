1ef8220d566dd597bcaf7420da157077d89b520e
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-01-30 15:45:09 +0100

    chore(storage): update production hub api routes

--- _nuxthub/server/api/_hub/bucket/[key].get.ts
+++ _nuxthub/server/api/_hub/bucket/[key].get.ts
@@ -1,4 +1,14 @@
+import { useValidatedParams, useValidatedQuery, z } from 'h3-zod'
+
 export default eventHandler(async (event) => {
-  // TODO
-  return {}
+  // TODO: handle authorization
+
+  const { name } = await useValidatedQuery(event, {
+    name: z.ostring()
+  })
+  const { key } = await useValidatedParams(event, {
+    key: z.string().min(1)
+  })
+
+  return useBlob(name).get(key)
 })
