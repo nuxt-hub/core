4331cc259e76c88cf839b7b4cce61c628fc83d76
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-02-01 17:29:57 +0100

    chore: lint config + indent

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,10 @@ module.exports = {
     // Vue
     'vue/multi-word-component-names': 0,
     'vue/max-attributes-per-line': 'off',
-    'vue/no-v-html': 0
+    'vue/no-v-html': 0,
+    'vue/html-indent': ['error', 2],
+    'vue/script-indent': ['error', 2, { baseIndent: 0 }],
+    // Typescript
+    '@typescript-eslint/indent': ['error', 2],
   }
 }
