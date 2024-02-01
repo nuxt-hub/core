aa6758e6408100b8a60ca82d27c1d02a0da7893e
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-02-01 12:54:15 +0100

    chore(blob): up

--- _nuxthub/server/api/_hub/blob/[...pathname].put.ts
+++ _nuxthub/server/api/_hub/blob/[...pathname].put.ts
@@ -1,18 +1,3 @@
-async function streamToArrayBuffer(stream: ReadableStream, streamSize: number) {
-  const result = new Uint8Array(streamSize)
-  let bytesRead = 0
-  const reader = stream.getReader()
-  while (true) {
-    const { done, value } = await reader.read()
-    if (done) {
-      break
-    }
-    result.set(value, bytesRead)
-    bytesRead += value.length
-  }
-  return result
-}
-
 export default eventHandler(async (event) => {
   const { pathname } = await getValidatedRouterParams(event, z.object({
     pathname: z.string().min(1)
@@ -21,17 +6,12 @@ export default eventHandler(async (event) => {
 
   const contentType = getHeader(event, 'content-type')
   const contentLength = Number(getHeader(event, 'content-length') || '0')
-  if (!query.contentType) {
-    query.contentType = contentType
-  }
-  if (!query.contentLength) {
-    query.contentLength = contentLength
-  }
 
-  // FIXME: find a way to re-stream the readable stream
-  // const body = getRequestWebStream(event)!
-  const stream = getRequestWebStream(event)!
-  const body = await streamToArrayBuffer(stream, contentLength)
+  const options = { ...query }
+  if (!options.contentType) { options.contentType = contentType }
+  if (!options.contentLength) { options.contentLength = contentLength }
 
-  return useBlob().put(pathname, body, query)
+  const body = getRequestWebStream(event)!
+
+  return useBlob().put(pathname, body, options)
 })
