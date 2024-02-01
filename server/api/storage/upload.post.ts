0ae27d8f4bfdb4201b7e7b199c8eea26c2681d2b
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-02-01 10:22:44 +0100

    chore: clean code

--- server/api/storage/upload.post.ts
+++ server/api/storage/upload.post.ts
@@ -1,16 +1,13 @@
 export default eventHandler(async (event) => {
-  console.log('before')
   const form = await readFormData(event)
-  console.log('getting file')
   const file = form.get('file') as Blob
-  console.log('after')
 
   if (!file || !file.size) {
-    throw createError({ status: 400, message: 'No file provided' })
+    throw createError({ statusCode: 400, message: 'No file provided' })
   }
 
   if (!file.type.startsWith('image/')) {
-    throw createError({ status: 400, message: 'File must be an image' })
+    throw createError({ statusCode: 400, message: 'File must be an image' })
   }
 
   return useBlob().put(`images/${file.name}`, file)
