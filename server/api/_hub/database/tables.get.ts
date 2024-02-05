33648221c81693d9e70e4f91db2c94245a0fc8ed
Sébastien Chopin <seb@nuxt.com>
2024-02-05 18:46:45 +0100

    feat: add primary keys and indexes

--- server/api/_hub/database/tables.get.ts
+++ server/api/_hub/database/tables.get.ts
@@ -20,17 +20,22 @@ export default eventHandler(async () => {
     return !isInternal
   })
 
-  const [columns, count] = await Promise.all([
+  const [columns, count, primaryKeys, indexes] = await Promise.all([
     db.batch(tables.map(({ name }) => db.prepare(`PRAGMA table_info("${name}")`)))
-      .then(res => res.map(({ results }) => results as { name: string; type: string }[])),
+      .then(res => res.map(({ results }) => results as { name: string; type: string, notnull: number, dflt_value: null | string, pk: number }[])),
     db.batch<{ c: number }>(tables.map(({ name }) => db.prepare(`SELECT COUNT(*) AS c FROM "${name}"`)))
-      .then(res => res.map(({ results }) => results[0].c))
+      .then(res => res.map(({ results }) => results[0].c)),
+    db.batch(tables.map(({ name }) => db.prepare(`PRAGMA foreign_key_list("${name}")`)))
+      .then(res => res.map(({ results }) => results as { name: string; type: string }[])),
+    db.batch(tables.map(({ name }) => db.prepare(`PRAGMA index_list("${name}")`)))
+      .then(res => res.map(({ results }) => results as { name: string; type: string }[])),
   ])
-
 
   return tables.map(({ name }, i) => ({
     name,
     columns: columns[i],
+    primaryKeys: primaryKeys[i],
+    indexes: indexes[i],
     count: count[i]
   }))
 })
