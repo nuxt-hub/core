92381d82bb6c3d87303f454af501040fa66b0269
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-02-14 15:53:14 +0100

    chore(analytics): update

--- _demo/plugins/cloudflare-beacon.client.ts
+++ _demo/plugins/cloudflare-beacon.client.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(() => {
   const cloudflareScript = document.createElement('script')
   cloudflareScript.setAttribute('src', 'https://static.cloudflareinsights.com/beacon.min.js')
-  cloudflareScript.setAttribute('defer', true)
+  cloudflareScript.setAttribute('defer', 'true')
   cloudflareScript.setAttribute('data-cf-beacon', '{"token": "cd0e6688bd0944719fe963c41759b992"}')
   document.body.appendChild(cloudflareScript)
 })
