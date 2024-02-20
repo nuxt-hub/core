a7133350682b82e625a918363b4c6c844d75add9
Sébastien Chopin <seb@nuxt.com>
2024-02-20 18:41:17 +0100

    chore(playground): add basic auth

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,12 +2,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '../src/module',
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@kgierke/nuxt-basic-auth'
   ],
   ui: {
     icons: ['heroicons', 'simple-icons']
   },
   hub: {
     // local: true
+  },
+  basicAuth: {
+    enabled: process.env.NODE_ENV === 'production',
+    users: [
+      {
+        username: 'admin',
+        password: process.env.NUXT_ADMIN_PASSWORD || 'admin'
+      }
+    ]
   }
 })
