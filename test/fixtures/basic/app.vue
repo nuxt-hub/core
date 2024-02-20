a33371f139e82137999222376f966faf1de5d7be
Sébastien Chopin <seb@nuxt.com>
2024-02-20 18:37:32 +0100

    chore(test): add missing import

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import { useFetch } from '#app'
+
 const { data: primitives } = useFetch('/api/_hub/primitives')
 </script>
 
 <template>
   <h3>Primitives enabled:</h3>
   <ul>
-    <li v-for="primitive in Object.keys(primitives)" :key="primitive">
+    <li v-for="primitive in Object.keys(primitives || {})" :key="primitive">
       {{ primitive }}
     </li>
   </ul>
