77116ca65425731c5a2da937f742a77317888606
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-02-20 14:52:46 +0100

    feat(docs): initial documentation

---
+++ docs/components/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex items-center gap-1">
+    <span>Nuxt</span>
+    <span class="rounded-md bg-primary-500 text-gray-900 px-0.5">Hub</span>
+  </div>
+</template>
---
+++ docs/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'DM Sans fallback', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  }
+}
