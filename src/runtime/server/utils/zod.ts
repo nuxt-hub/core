191269a5e01822e0199ac44cd56d53602b138d94
Sébastien Chopin <seb@nuxt.com>
2024-02-21 17:30:18 +0100

    chore(types): improve extended zod types

--- src/runtime/server/utils/zod.ts
+++ src/runtime/server/utils/zod.ts
@@ -1,6 +1,11 @@
 import { z as zod } from 'zod'
 
-export const z = {
+interface ExtendedZodMethods {
+  intAsString: () => zod.ZodEffects<zod.ZodString, number, string>
+}
+type ExtendedZod = typeof zod & ExtendedZodMethods
+
+export const z: ExtendedZod = {
   ...zod,
   intAsString: () => zod.string().transform(val => parseInt(val, 10))
 }
