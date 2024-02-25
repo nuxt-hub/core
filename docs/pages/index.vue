377f0be7ad4c71a22985c0474426dad4291d6d01
Sébastien Chopin <seb@nuxt.com>
2024-02-26 00:49:10 +0100

    docs: fix lint

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -16,32 +16,36 @@ defineOgImage({
 </script>
 
 <template>
-  <ULandingHero v-bind="page.hero">
-    <template #top><Background class="opacity-30" /></template>
-    <template #headline>
-      <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="relative rounded-full font-semibold">
-        <NuxtLink :to="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
-          <span class="absolute inset-0" aria-hidden="true" />
-        </NuxtLink>
+  <div>
+    <ULandingHero v-bind="page.hero">
+      <template #top>
+        <Background class="opacity-30" />
+      </template>
+      <template #headline>
+        <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="relative rounded-full font-semibold">
+          <NuxtLink :to="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
+            <span class="absolute inset-0" aria-hidden="true" />
+          </NuxtLink>
 
-        {{ page.hero.headline.label }}
+          {{ page.hero.headline.label }}
 
-        <UIcon v-if="page.hero.headline.icon" :name="page.hero.headline.icon" class="ml-1 w-4 h-4 pointer-events-none" />
-      </UBadge>
-    </template>
+          <UIcon v-if="page.hero.headline.icon" :name="page.hero.headline.icon" class="ml-1 w-4 h-4 pointer-events-none" />
+        </UBadge>
+      </template>
 
-    <template #title>
-      <MDC :value="page.hero.title" />
-    </template>
+      <template #title>
+        <MDC :value="page.hero.title" />
+      </template>
 
-    <MDC :value="page.hero.code" tag="pre" class="hero_code prose prose-primary dark:prose-invert lg:ml-auto" />
-  </ULandingHero>
+      <MDC :value="page.hero.code" tag="pre" class="hero_code prose prose-primary dark:prose-invert lg:ml-auto" />
+    </ULandingHero>
 
-  <ULandingSection :title="page.features.title" :description="page.features.description" :links="page.features.links" class="pt-0 sm:pt-0">
-    <UPageGrid>
-      <ULandingCard v-for="(item, index) of page.features.items" :key="index" v-bind="item" />
-    </UPageGrid>
-  </ULandingSection>
+    <ULandingSection :title="page.features.title" :description="page.features.description" :links="page.features.links" class="pt-0 sm:pt-0">
+      <UPageGrid>
+        <ULandingCard v-for="(item, index) of page.features.items" :key="index" v-bind="item" />
+      </UPageGrid>
+    </ULandingSection>
+  </div>
 </template>
 
 <style lang="postcss">
