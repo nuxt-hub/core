d1f0942a41e040db0d34d0bd7c5cf0c41a31c8a3
Sébastien Chopin <seb@nuxt.com>
2024-02-27 23:42:17 +0100

    docs: add prose-a in tip component

--- docs/components/content/Tip.vue
+++ docs/components/content/Tip.vue
@@ -3,7 +3,7 @@
     title="Tip"
     icon="i-ph-lightbulb-duotone"
     color="green"
-    class="dark:!bg-green-800/40 !bg-green-200/40 dark:!border-green-600/30 !border-green-400/30"
+    class="prose-a:text-green-500 hover:prose-a:border-green-500 dark:prose-a:text-green-400 dark:hover:prose-a:border-green-400 dark:!bg-green-800/40 !bg-green-200/40 dark:!border-green-600/30 !border-green-400/30"
     :class="$attrs.to ? 'dark:hover:!border-green-600/50 hover:!border-green-400/50' : ''"
   >
     <ContentSlot :use="$slots.default" unwrap="p" />
