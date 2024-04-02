99bd585ff720c9f07a7b9a500f5ca7132c3522ba
Farnabaz <farnabaz@gmail.com>
2024-04-02 10:24:46 +0200

    fix: apply review

--- src/module/runtime/server/api/_hub/openapi.get.ts
+++ src/module/runtime/server/api/_hub/openapi.get.ts
@@ -1,4 +1,26 @@
-import openAPIJSON from '#internal/nitro/routes/openapi'
+import { useRuntimeConfig } from '#imports'
 
-export default openAPIJSON
+export default eventHandler(async (event) => {
+  const hub = useRuntimeConfig().hub
+
+  if (!hub.openapi) {
+    throw createError({
+      statusCode: 422,
+      message: 'OpenAPI not configured'
+    })
+  }
+
+  const openapi = await import('#internal/nitro/routes/openapi')
+    .then((mod) => mod.default)
+    .catch(() => undefined)
+  
+  if (typeof openapi !== 'function') {
+    throw createError({
+      statusCode: 404,
+      message: 'not found'
+    })
+  }
+
+  return openapi(event)
+})
 
