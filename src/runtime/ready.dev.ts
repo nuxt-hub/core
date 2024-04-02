4933bc8dc5751af592f00afb3cd7ea2ad66359e9
Farnabaz <farnabaz@gmail.com>
2024-04-02 12:15:29 +0200

    fix: missing imports

--- src/runtime/ready.dev.ts
+++ src/runtime/ready.dev.ts
@@ -1,4 +1,5 @@
 import { defineNitroPlugin } from 'nitropack/runtime/plugin'
+import { hubHooks } from './server/utils/hooks'
 
 export default defineNitroPlugin(async () => {
   // Wait for nitro-cloudflare-dev to be ready
