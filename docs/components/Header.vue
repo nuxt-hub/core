90867dbe2dfea85e5f6495ab8274a3b48f8282f5
Florent Delerue <florentdelerue@hotmail.com>
2024-04-09 17:38:03 +0200

    feat(header): up

--- docs/components/Header.vue
+++ docs/components/Header.vue
@@ -34,16 +34,16 @@ const links = [
       <UTooltip text="Search" :shortcuts="[metaSymbol, 'K']" :popper="{ strategy: 'absolute' }">
         <UContentSearchButton :label="null" />
       </UTooltip>
-      <UButton variant="ghost" label="Log in" to="/login" color="black" size="md" class="hidden sm:block" external />
-      <UButton variant="solid" label="Sign up" to="/signup" size="md" class="text-gray-900 bg-green-400 hidden sm:block" external />
+      <UButton variant="ghost" label="Log in" to="https://admin.hub.nuxt.com/" color="black" size="md" class="hidden sm:block" external />
+      <UButton variant="solid" label="Sign up" to="https://admin.hub.nuxt.com/" size="md" class="text-gray-900 bg-green-400 hidden sm:block" external />
     </template>
 
     <template #panel>
       <UNavigationTree :links="links" />
 
       <div class="flex flex-col gap-y-2 mt-4">
-        <UButton variant="solid" label="Log in" to="/login" color="white" size="md" class="flex justify-center" external />
-        <UButton variant="solid" label="Sign up" to="/signup" size="md" class="flex justify-center text-gray-900 bg-primary" external />
+        <UButton variant="solid" label="Log in" to="https://admin.hub.nuxt.com/" color="white" size="md" class="flex justify-center sm:hidden" external />
+        <UButton variant="solid" label="Sign up" to="https://admin.hub.nuxt.com/" size="md" class="flex justify-center text-gray-900 bg-primary sm:hidden" external />
       </div>
     </template>
   </UHeader>
