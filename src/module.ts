8c69740e2b1de6d8b01b999fa436522153cc904d
Sébastien Chopin <seb@nuxt.com>
2024-04-11 22:59:26 +0200

    chore(types): remove ts ignore

--- src/module.ts
+++ src/module.ts
@@ -96,7 +96,6 @@ export default defineNuxtModule<ModuleOptions>({
     let remoteArg = parseArgs(argv, { remote: { type: 'string' } }).remote as string
     remoteArg = (remoteArg === '' ? 'true' : remoteArg)
     const runtimeConfig = nuxt.options.runtimeConfig
-    // @ts-ignore
     const hub = runtimeConfig.hub = defu(runtimeConfig.hub || {}, options, {
       // Self-hosted project
       projectUrl: process.env.NUXT_HUB_PROJECT_URL || '',
@@ -121,7 +120,6 @@ export default defineNuxtModule<ModuleOptions>({
     // validate remote option
     if (hub.remote && !['true', 'production', 'preview'].includes(String(hub.remote))) {
       log.error('Invalid remote option, should be `false`, `true`, `\'production\'` or `\'preview\'`')
-      // @ts-ignore
       delete hub.remote
     }
     // Log when using a different Hub url
@@ -158,15 +156,14 @@ export default defineNuxtModule<ModuleOptions>({
 
     // Within CF Pages CI/CD to notice NuxtHub about the build and hub config
     if (!nuxt.options.dev && process.env.CF_PAGES && process.env.NUXT_HUB_PROJECT_DEPLOY_TOKEN && process.env.NUXT_HUB_PROJECT_KEY && process.env.NUXT_HUB_ENV) {
-      nuxt.hook('modules:done', async () => {
-        const { bindingsChanged } = await $fetch<{ bindingsChanged: boolean }>(`/api/projects/${process.env.NUXT_HUB_PROJECT_KEY}/build/${process.env.NUXT_HUB_ENV}/before`, {
+      nuxt.hook('build:before', async () => {
+        await $fetch(`/api/projects/${process.env.NUXT_HUB_PROJECT_KEY}/build/${process.env.NUXT_HUB_ENV}/before`, {
           baseURL: hub.url,
           method: 'POST',
           headers: {
             authorization: `Bearer ${process.env.NUXT_HUB_PROJECT_DEPLOY_TOKEN}`
           },
           body: {
-            pagesUrl: process.env.CF_PAGES_URL,
             analytics: hub.analytics,
             blob: hub.blob,
             cache: hub.cache,
@@ -182,18 +179,6 @@ export default defineNuxtModule<ModuleOptions>({
 
           process.exit(1)
         })
-
-        if (bindingsChanged) {
-          log.box([
-            'NuxtHub detected some changes in this project bindings and updated your Pages project on your Cloudflare account.',
-            'In order to enable this changes, this deployment will be cancelled and a new one has been created.'
-          ].join('\n'))
-
-          // Wait 2 seconds to make sure NuxtHub cancel the deployment before exiting
-          await new Promise((resolve) => setTimeout(resolve, 2000))
-
-          process.exit(1)
-        }
       })
 
       nuxt.hook('build:done', async () => {
@@ -272,7 +257,6 @@ export default defineNuxtModule<ModuleOptions>({
           process.exit(1)
         }
         // Update hub.env in runtimeConfig
-        // @ts-ignore
         hub.env = env
       }
 
