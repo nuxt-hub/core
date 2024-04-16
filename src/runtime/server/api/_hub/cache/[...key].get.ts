5bc98a793f3b5cfce8bbefe349237fd73ddfeacb
Farnabaz <farnabaz@gmail.com>
2024-04-16 17:09:17 +0200

    feat: cache management (#73)
    * feat: cache management
    
    * up
    
    * Update src/runtime/server/utils/cache.ts
    
    * chore: pnpm v9
    
    ---------
    
    Co-authored-by: Sébastien Chopin <seb@nuxt.com>

---
+++ src/runtime/server/api/_hub/cache/[...key].get.ts
@@ -0,0 +1,49 @@
+import ms from 'ms'
+import { eventHandler, getRouterParam } from 'h3'
+import { requireNuxtHubAuthorization } from '../../../utils/auth'
+import { requireNuxtHubFeature } from '../../../utils/features'
+// @ts-ignore
+import { useStorage } from '#imports'
+
+export default eventHandler(async (event) => {
+  await requireNuxtHubAuthorization(event)
+  requireNuxtHubFeature('cache')
+
+  const key = getRouterParam(event, 'key') || ''
+  // If ends with an extension
+  if (/\.([a-z0-9]+)$/i.test(key)) {
+    return await useStorage('cache:nitro').getItem(key)
+  }
+  const storage = useStorage(`cache:nitro:${key}`)
+  const keys = await storage.getKeys()
+
+  const stats = {
+    groups: {} as Record<string, number>,
+    cache: [] as any[]
+  }
+
+  await Promise.all(keys.map(async (key: string) => {
+    if (key.includes(':')) {
+      const k = key.split(':')[0]
+      stats.groups[k] = (stats.groups[k] || 0) + 1
+      return
+    }
+    const item = await storage.getItem(key)
+    delete item.value
+
+    const entry: any = {
+      key,
+      ...item,
+      size: JSON.stringify(item).length
+    }
+    try {
+      entry.duration = ms(item.expires - item.mtime, { long: true })
+    } catch (err) {
+      entry.duration = 'unknown'
+      console.log('Cannot have ms duration', key, err)
+    }
+    stats.cache.push(entry)
+  }))
+
+  return stats
+})
