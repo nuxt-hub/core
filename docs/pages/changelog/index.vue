d7f2e066b4fae60fb484e371e7b442d5d8497cc7
Florent Delerue <florentdelerue@hotmail.com>
2024-04-17 12:16:25 +0200

    feat(changelog): timeline improvements

--- docs/pages/changelog/index.vue
+++ docs/pages/changelog/index.vue
@@ -16,6 +16,9 @@ const { y } = useWindowScroll()
 const { isScrolling, arrivedState } = useScroll(document)
 const { url } = useSiteConfig()
 
+
+
+
 useSeoMeta({
   title: page.value.title,
   ogTitle: page.value.title,
@@ -24,8 +27,8 @@ useSeoMeta({
   ogImage: joinURL(url, '/social-card.png')
 })
 
-onMounted(() => { 
-  dot.value.style.left = `${container.value[0].offsetLeft + 3 }px`
+onMounted(() => {
+  dot.value.style.left = `${container.value[0].offsetLeft + 3}px`
 })
 
 watch(() => y.value, () => {
@@ -37,16 +40,31 @@ watch(() => y.value, () => {
     return point.getBoundingClientRect().top + window.scrollY
   })
 
-  for (let i = 0; i < fixedPoints.length; i++) {
-    if (Math.abs(mobilePointTop - fixedPoints[i]) <= 100) {
-      dots.value[i].classList.add('neon')
-      clotherPoint.value = true
-    } else {
-      if (dots.value[i].classList.value.includes('neon')) {
-        dots.value[i].classList.remove('neon')
-        clotherPoint.value = false
+  if (!(arrivedState.top || arrivedState.bottom)) {
+    for (let i = 0; i < fixedPoints.length; i++) {
+      if (Math.abs(mobilePointTop - fixedPoints[i]) <= 100) {
+        dots.value[i].classList.add('neon')
+        clotherPoint.value = true
+      } else {
+        if (dots.value[i].classList.value.includes('neon')) {
+          dots.value[i].classList.remove('neon')
+          clotherPoint.value = false
+        }
       }
     }
+  }
+})
+
+watch(() => arrivedState.top, () => {
+  if (arrivedState.top === true) {
+    dots.value[0].classList.add('neon')
+  }
+})
+watch(() => arrivedState.bottom, () => {
+
+  console.log('arrivedState', arrivedState.bottom)
+  if (arrivedState.bottom === true) {
+    dots.value[dots.value.length - 1].classList.add('neon')
   }
 })
 
@@ -55,18 +73,20 @@ await fetchList()
 
 <template>
   <UContainer v-if="page">
-    <UPageHero v-bind="page?.hero" :ui="{ wrapper: 'border-none' }"/>
+    <UPageHero v-bind="page?.hero" :ui="{ wrapper: 'border-none' }" />
     <ul class="flex flex-col relative">
       <div ref="dot" class="hidden lg:block absolute w-[2px] rounded-full bg-gray-500 dark:bg-gray-400 z-10 neon dot"
-        :style="{ top: `${scrollTop}px`, height: `${isScrolling ? 40 : clotherPoint ? 0 : 40}px`, width: `${isScrolling ? 1 : clotherPoint ? 0 : 1}px` }" />
-      <li v-for="(changelog, index) in changelogs" :key="changelog.title" class="relative flex w-full flex-col lg:flex-row last:mb-[2px] group">
+        :style="{ top: `${scrollTop}px`, height: `${isScrolling ? 40 : (clotherPoint || arrivedState.top || arrivedState.bottom) ? 0 : 40}px`, width: `${isScrolling ? 1 : clotherPoint ? 0 : 1}px` }" />
+      <li v-for="(changelog, index) in changelogs" :key="changelog.title"
+        class="relative flex w-full flex-col lg:flex-row last:mb-[2px] group">
         <div class="flex w-full pb-4 lg:w-[200px] lg:pb-0 -mt-1">
           <p class="text-sm text-gray-600 dark:text-gray-300">
             <time class="top-24">{{ formatDateByLocale('en', changelog.date) }}</time>
           </p>
         </div>
         <div ref="container" class="relative hidden lg:flex lg:min-w-[150px] lg:w-[150px]">
-          <div class="hidden lg:block left-4 top-24 h-2 w-2 rounded-full bg-gray-500 dark:bg-gray-400 z-10" :class="{ 'neon': index === 0 }" ref="dots" />
+          <div class="hidden lg:block left-4 top-24 h-2 w-2 rounded-full bg-gray-500 dark:bg-gray-400 z-10"
+            :class="{ 'neon': index === 0 }" ref="dots" />
 
           <div class="absolute left-[3.5px] top-0.5 h-full w-[1px] bg-gray-400 dark:bg-gray-500" />
         </div>
