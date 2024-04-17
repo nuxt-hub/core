29fce61614b34020a2758e42f9ea0b02e3e79998
Gerben <gerbenmulder2508@gmail.com>
2024-04-17 21:01:12 +0200

    fix: add cache to manifest

--- src/runtime/server/api/_hub/manifest.get.ts
+++ src/runtime/server/api/_hub/manifest.get.ts
@@ -1,5 +1,5 @@
 import { eventHandler } from 'h3'
-import { useRuntimeConfig } from '#imports'
+import { useRuntimeConfig, useStorage } from '#imports'
 import { hubDatabase } from '../../utils/database'
 import { hubKV } from '../../utils/kv'
 import { hubBlob } from '../../utils/blob'
@@ -8,10 +8,11 @@ import { requireNuxtHubAuthorization } from '../../utils/auth'
 export default eventHandler(async (event) => {
   await requireNuxtHubAuthorization(event)
   const { version } = useRuntimeConfig().hub
-  const [ dbCheck, kvCheck, blobCheck ] = await Promise.all([
+  const [ dbCheck, kvCheck, blobCheck, cacheCheck ] = await Promise.all([
     falseIfFail(() => hubDatabase().exec('PRAGMA table_list')),
     falseIfFail(() => hubKV().getKeys('__check__')),
-    falseIfFail(() => hubBlob().list({ prefix: '__check__' }))
+    falseIfFail(() => hubBlob().list({ prefix: '__check__' })),
+    falseIfFail(() => useStorage('cache:nitro').getKeys('__check__'))
   ])
 
   return {
@@ -19,7 +20,8 @@ export default eventHandler(async (event) => {
     storage: {
       database: Boolean(dbCheck),
       kv: Array.isArray(kvCheck),
-      blob: Array.isArray(blobCheck)
+      blob: Array.isArray(blobCheck),
+      cache: Array.isArray(cacheCheck),
     }
   }
 })
