16a363a1537d816c7f758973ff99521a15bd2498
Rihan <me@file.properties>
2024-06-20 12:37:52 +0000

    feat: vectorize via proxy

---
+++ src/runtime/vectorize/api/_hub/vectorize/[command].post.ts
@@ -0,0 +1,53 @@
+import { eventHandler, getValidatedRouterParams, readValidatedBody } from 'h3'
+import { z } from 'zod'
+import { hubVectorize } from '../../../server/utils/vectorize'
+import { requireNuxtHubAuthorization } from '../../../../utils/auth'
+import { requireNuxtHubFeature } from '../../../../utils/features'
+
+export default eventHandler(async (event) => {
+  await requireNuxtHubAuthorization(event)
+  requireNuxtHubFeature('vectorize')
+
+  // https://developers.cloudflare.com/vectorize/reference/client-api/
+  const { command } = await getValidatedRouterParams(event, z.object({
+    command: z.enum(['insert', 'upsert', 'query', 'getByIds', 'deleteByIds', 'describe'])
+  }).parse)
+  const vectorize = hubVectorize()
+
+  if (command === 'insert' || command === 'upsert') {
+    const { vectors } = await readValidatedBody(event, z.object({
+      vectors: z.array(z.object({
+        id: z.string().min(1).max(256),
+        namespace: z.string().optional(),
+        values: z.array(z.number()),
+        metadata: z.record(z.string(), z.any()).optional()
+      }))
+    }).parse)
+    return vectorize[command](vectors)
+  }
+
+  if (command === 'query') {
+    const { query, params } = await readValidatedBody(event, z.object({
+      query: z.array(z.number()),
+      params: z.object({
+        topK: z.number().optional(),
+        namespace: z.string().optional(),
+        returnValues: z.boolean().optional(),
+        returnMetadata: z.boolean().optional(),
+        filter: z.record(z.string(), z.any()).optional()
+      })
+    }).parse)
+    return vectorize.query(query, params)
+  }
+
+  if (command === 'getByIds' || command === 'deleteByIds') {
+    const { ids } = await readValidatedBody(event, z.object({
+      ids: z.array(z.string().min(1).max(256))
+    }).parse)
+    return vectorize[command](ids)
+  }
+
+  if (command === 'describe') {
+    return vectorize.describe()
+  }
+})
--- src/runtime/vectorize/server/utils/vectorize.ts
+++ src/runtime/vectorize/server/utils/vectorize.ts
@@ -1,6 +1,10 @@
+import { ofetch } from 'ofetch'
+import { joinURL } from 'ufo'
 import { createError } from 'h3'
-import type { VectorizeIndex } from '@nuxthub/core'
+import type { H3Error } from 'h3'
+import type { VectorizeIndex } from '../../../../types/vectorize'
 import { requireNuxtHubFeature } from '../../../utils/features'
+import { useRuntimeConfig } from '#imports'
 
 let _vectorize: VectorizeIndex
 
@@ -16,6 +20,8 @@ let _vectorize: VectorizeIndex
  * let inserted = await vectorize.insert(vectorsToInsert)
  * ```
  *
+ * Vectorize is currently only supported with `--remote`. See https://developers.cloudflare.com/workers/testing/local-development/#supported-resource-bindings-in-different-environments for details.
+ *
  * @see https://developers.cloudflare.com/vectorize/reference/client-api/
  */
 export function hubVectorize(): VectorizeIndex {
@@ -24,11 +30,78 @@ export function hubVectorize(): VectorizeIndex {
   if (_vectorize) {
     return _vectorize
   }
+  const hub = useRuntimeConfig().hub
   // @ts-expect-error globalThis.__env__ is not defined
   const binding = process.env.VECTORIZE || globalThis.__env__?.VECTORIZE || globalThis.VECTORIZE
+  if (hub.remote && hub.projectUrl && !binding) {
+    _vectorize = proxyHubVectorize(hub.projectUrl, hub.projectSecretKey || hub.userToken)
+    return _vectorize
+  }
   if (binding) {
     _vectorize = binding as VectorizeIndex
     return _vectorize
   }
   throw createError('Missing Cloudflare Vectorize binding (Vectorize)')
+}
+
+/**
+ * Access the remote Vectorize database.
+ *
+ * @param projectUrl The project URL (e.g. https://my-deployed-project.nuxt.dev)
+ * @param secretKey The secret key to authenticate to the remote endpoint
+ *
+ * @example ```ts
+ * const db = proxyHubVectorize('https://my-deployed-project.nuxt.dev', 'my-secret-key')
+ * let vectorsToInsert = [
+ *   {id: "123", values: [32.4, 6.5, 11.2, 10.3, 87.9]},
+ *   {id: "456", values: [2.5, 7.8, 9.1, 76.9, 8.5]},
+ * ]
+ * let inserted = await vectorize.insert(vectorsToInsert)
+ * ```
+ *
+ * @see https://developers.cloudflare.com/vectorize/reference/client-api/
+ */
+export function proxyHubVectorize(projectUrl: string, secretKey?: string): VectorizeIndex {
+  requireNuxtHubFeature('vectorize')
+
+  const vectorizeAPI = ofetch.create({
+    baseURL: joinURL(projectUrl, '/api/_hub/vectorize'),
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${secretKey}`
+    }
+  })
+  return {
+    async insert(vectors: { id: string, namespace?: string, values: number[], metadata?: Record<string, unknown> }[]) {
+      return vectorizeAPI('/insert', { body: { vectors } }).catch(handleProxyError)
+    },
+
+    async upsert(vectors: { id: string, namespace?: string, values: number[], metadata?: Record<string, unknown> }[]) {
+      return vectorizeAPI('/upsert', { body: { vectors } }).catch(handleProxyError)
+    },
+
+    async query(query: number[], params: { topK?: number, namespace?: string, returnValues?: boolean, returnMetadata?: boolean, filter?: Record<string, unknown> }) {
+      return vectorizeAPI('/query', { body: { query, params } }).catch(handleProxyError)
+    },
+
+    async getByIds(ids: string[], namespace?: string) {
+      return vectorizeAPI('/getByIds', { body: { ids, namespace } }).catch(handleProxyError)
+    },
+
+    async deleteByIds(ids: string[], namespace?: string) {
+      return vectorizeAPI('/deleteByIds', { body: { ids, namespace } }).catch(handleProxyError)
+    },
+
+    async describe() {
+      return vectorizeAPI('/describe').catch(handleProxyError)
+    }
+  } as VectorizeIndex
+}
+
+function handleProxyError(err: H3Error) {
+  throw createError({
+    statusCode: err.statusCode,
+    // @ts-expect-error not aware of data property
+    message: err.data?.message || err.message
+  })
 }
