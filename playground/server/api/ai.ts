695071178adf69a3c1de6512883628aaeb532642
Rihan <me@file.properties>
2024-06-27 20:34:26 +0000

    chore: update model used in playground example prior model is deprecated

--- playground/server/api/ai.ts
+++ playground/server/api/ai.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async () => {
   const ai = hubAi()
-  return await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+  return await ai.run('@cf/meta/llama-3-8b-instruct', {
     prompt: 'Who is the author of Nuxt?'
   })
 })
