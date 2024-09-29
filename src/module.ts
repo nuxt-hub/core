ff1a7abdf1ef6575edadc0b9769ed12ba9d9c75c
Sébastien Chopin <seb@nuxt.com>
2024-09-29 15:45:32 +0200

    chore: read and ignore nitro wrangler option

--- src/module.ts
+++ src/module.ts
@@ -71,6 +71,15 @@ export default defineNuxtModule<ModuleOptions>({
       }
     })
     runtimeConfig.hub = hub
+    // Make sure to tell Nitro to not generate the wrangler.toml file
+    // @ts-expect-error nitro.cloudflare.wrangler is not yet typed
+    delete nuxt.options.nitro.cloudflare?.wrangler?.compatibility_flags
+    // @ts-expect-error nitro.cloudflare.wrangler is not yet typed
+    if (nuxt.options.nitro.cloudflare?.wrangler && Object.keys(nuxt.options.nitro.cloudflare.wrangler).length) {
+      log.warn('The `nitro.cloudflare.wrangler` option is not supported by NuxtHub, ignoring it...')
+      // @ts-expect-error nitro.cloudflare.wrangler is not yet typed
+      nuxt.options.nitro.cloudflare.wrangler = {}
+    }
     // validate remote option
     if (hub.remote && !['true', 'production', 'preview'].includes(String(hub.remote))) {
       log.error('Invalid remote option, should be `false`, `true`, `\'production\'` or `\'preview\'`')
