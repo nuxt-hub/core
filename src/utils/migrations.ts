8f22d22fe88e776352309b86c61a98daefbc6275
Rihan Arfan <me@file.properties>
2024-10-23 06:24:10 +0100

    feat: apply migrations in pages ci

--- src/utils/migrations.ts
+++ src/utils/migrations.ts
@@ -7,8 +7,10 @@ import { useRuntimeConfig, logger } from '@nuxt/kit'
 const log = logger.withTag('nuxt:hub')
 
 export const runMigrations = async () => {
+  const srcStorage = useMigrationsStorage()
   await createMigrationsTable()
 
+  log.info('Checking for pending migrations...')
   const remoteMigrations = await getRemoteMigrations().catch((error) => {
     log.error(`Could not retrieve migrations on ${process.env.NUXT_HUB_ENV}.`)
     if (error) log.error(error)
@@ -17,9 +19,29 @@ export const runMigrations = async () => {
   if (!remoteMigrations.length) log.warn(`No applied migrations on ${process.env.NUXT_HUB_ENV}.`)
 
   const localMigrations = (await getMigrationFiles()).map(fileName => fileName.replace('.sql', ''))
-
   const pendingMigrations = localMigrations.filter(localName => !remoteMigrations.find(({ name }) => name === localName))
-  if (!pendingMigrations.length) log.info('No pending migrations to apply.')
+  if (!pendingMigrations.length) return log.info('No pending migrations to apply.')
+
+  log.info('Applying migrations...')
+  for (const migration of pendingMigrations) {
+    const migrationFile = await srcStorage.getItemRaw(`${migration}.sql`)
+    let query = migrationFile.toString()
+
+    if (query.at(-1) !== ';') query += ';' // ensure previous statement ended before running next query
+    query += `
+      INSERT INTO hub_migrations (name) values ('${migration}');
+    `
+
+    try {
+      await useDatabaseQuery(query)
+    } catch (error: any) {
+      log.error(`Failed to apply migration \`${migration}\`.`)
+      if (error && error.response) log.error(error.response?._data?.message || error)
+      break
+    }
+
+    log.success(`Applied migration \`${migration}\`.`)
+  }
 }
 
 export const useDatabaseQuery = async <T>(query: string) => {
