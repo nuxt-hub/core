f45a682afc5ce82819640dd7bd4d8d0cd50f56dc
Sébastien Chopin <seb@nuxt.com>
2024-11-18 14:48:27 +0100

    chore(playground): add password for invoice

--- playground/server/routes/invoice.pdf.ts
+++ playground/server/routes/invoice.pdf.ts
@@ -1,6 +1,10 @@
 export default eventHandler(async (event) => {
   const { page } = await hubBrowser()
-  await page.goto(`${getRequestURL(event).origin}/_invoice`)
+  let url = getRequestURL(event).origin
+  if (!import.meta.dev) {
+    url = url.replace('https://', `https://admin:${process.env.NUXT_ADMIN_PASSWORD || 'admin'}@`)
+  }
+  await page.goto(`${url}/_invoice`)
 
   setHeader(event, 'Content-Type', 'application/pdf')
   return page.pdf({ format: 'A4' })
