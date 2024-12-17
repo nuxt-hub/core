5a29f7d93d17c97b3fd2acb8d35c04bd538445e7
Sébastien Chopin <atinux@gmail.com>
2024-12-17 19:54:03 +0100

    fix(vectorize): set to undefined instead of throwing

--- playground/server/api/vectorize/index.get.ts
+++ playground/server/api/vectorize/index.get.ts
@@ -2,6 +2,5 @@ export default eventHandler(async (event) => {
   const { query } = await getValidatedQuery(event, z.object({
     query: z.array(z.number())
   }).parse)
-  const index = hubVectorize('example')
-  return index.query(query)
+  return hubVectorize('example')?.query(query)
 })
