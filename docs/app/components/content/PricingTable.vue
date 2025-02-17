d1bc6146c954e6383beea99fe47b7177148cef61
HugoRCD <hugo.richard@epitech.eu>
2025-02-17 19:06:30 +0100

    up

--- docs/app/components/content/PricingTable.vue
+++ docs/app/components/content/PricingTable.vue
@@ -4,6 +4,7 @@ defineProps<{
 }>()
 
 const { data: table } = await useAsyncData('pricing-table', () => queryCollection('cloudflarePricing').first())
+// TODO: Fix hydration issue
 </script>
 
 <template>
@@ -17,8 +18,8 @@ const { data: table } = await useAsyncData('pricing-table', () => queryCollectio
     }"
   >
     <template v-for="plan of table?.plans" :key="plan.label" #[plan.slot]="{ item }">
-      <ProseTable class="rounded-sm">
-        <ProseThead>
+      <ProseTable>
+        <ProseThead class="bg-transparent">
           <ProseTh v-for="(column, index) of item.columns" :key="index">
             {{ column.label }}
           </ProseTh>
@@ -26,32 +27,20 @@ const { data: table } = await useAsyncData('pricing-table', () => queryCollectio
         <ProseTbody>
           <ProseTr v-for="(row, index) of item.rows" :key="index">
             <ProseTd>
-              {{ row.title }}
+              <MDC :value="row.title" />
             </ProseTd>
             <ProseTd>
-              {{ row.free }}
+              <MDC :value="row.free" />
             </ProseTd>
             <ProseTd>
-              {{ row.paid }}
+              <MDC :value="row.paid" />
             </ProseTd>
           </ProseTr>
         </ProseTbody>
       </ProseTable>
-      <!--      <UTable :data="item.rows" :columns="item.columns" /> -->
-      <!--      <ProseTable
-        v-bind="item"
-        class="border dark:border-gray-800 border-gray-200 rounded-lg"
-        :ui="{
-          divide: 'dark:divide-gray-800 divide-gray-200'
-        }"
-      >
-        <template #paid-data="{ row }">
-          <span v-html="row.paid" />
-        </template>
-      </ProseTable>
       <div v-if="item.buttons?.length" class="mt-2 flex items-center gap-2 justify-center">
-        <UButton v-for="button of item.buttons" :key="button.to" v-bind="button" color="gray" size="xs" variant="link" trailing-icon="i-lucide-arrow-up-right" target="_blank" />
-      </div> -->
+        <UButton v-for="button of item.buttons" :key="button.to" v-bind="button" color="neutral" size="xs" variant="link" trailing-icon="i-lucide-arrow-up-right" target="_blank" />
+      </div>
     </template>
   </UTabs>
 </template>
