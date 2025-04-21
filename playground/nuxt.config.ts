865415d8f97bec63d901cb4318ea82ba130c4137
Rihan Arfan <me@file.properties>
2025-04-22 00:37:33 +0100

    fix: compatibility date

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
   },
   future: { compatibilityVersion: 4 },
 
-  compatibilityDate: '2025-05-18',
+  compatibilityDate: '2025-04-18',
 
   nitro: {
     // preset: 'cloudflare-durable',
