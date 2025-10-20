39171518960b96fbf42fe6bbb45e3601e5629f4a
Sébastien Chopin <atinux@gmail.com>
2025-10-20 21:29:48 +0200

    Update db.ts

--- playground/server/api/tests/db.ts
+++ playground/server/api/tests/db.ts
@@ -1,22 +1,17 @@
 export default defineEventHandler(async () => {
+  if (useRuntimeConfig().hub.database.dialect !== 'sqlite') {
+    return {
+      error: 'SQLite is not enabled',
+    }
+  }
   const db = useDrizzle()
 
-  const drizzleTables = await db.execute(sql`
-    SELECT
-      name,
-      type
-    FROM
-      sqlite_schema
-    WHERE
-      type = 'table' AND
-      name NOT LIKE 'sqlite_%' and name NOT LIKE '_litestream_%' and name NOT LIKE '__drizzle%'
-    ;
-  `)
+  const { rows } = await db.run(`SELECT name, type FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%' and name NOT LIKE '_litestream_%' and name NOT LIKE '__drizzle%';`)
 
-  const all = await db.select().from(tables.todos).limit(3)
+  const todos = await db.select().from(tables.todos).limit(3)
 
   return {
-    drizzleTables: drizzleTables.rows || drizzleTables,
-    all
+    drizzleTables: rows,
+    todos,
   }
 })
