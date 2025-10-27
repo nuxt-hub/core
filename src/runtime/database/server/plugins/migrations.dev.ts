509e25e119c84560f38fc4087a844d2aefccf3bc
Sébastien Chopin <atinux@gmail.com>
2025-10-27 23:42:26 +0100

    fix missingimport

--- src/runtime/database/server/plugins/migrations.dev.ts
+++ src/runtime/database/server/plugins/migrations.dev.ts
@@ -3,12 +3,11 @@ import { applyDatabaseMigrations, applyDatabaseQueries } from '../utils/migratio
 // @ts-expect-error - Generated at runtime
 import { drizzle } from 'hub:database'
 // @ts-expect-error - Generated at runtime
-import { defineNitroPlugin } from '#imports'
+import { defineNitroPlugin, useRuntimeConfig } from '#imports'
 
 export default defineNitroPlugin(async (nitroApp: any) => {
   if (!import.meta.dev) return
 
-  // @ts-expect-error - Nitro global
   const hub = useRuntimeConfig().hub as ResolvedHubConfig
   if (!hub.database) return
 
