4cc1ccc4c9134f9da74585bf6478c2cf342bdbf6
Sébastien Chopin <atinux@gmail.com>
2025-11-26 10:44:51 +0100

    update

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,5 +1,3 @@
-import yaml from '@rollup/plugin-yaml'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
@@ -62,11 +60,6 @@ export default defineNuxtConfig({
       // For CF trailing slash issue
       autoSubfolderIndex: false
     }
-  },
-  vite: {
-    plugins: [
-      yaml()
-    ]
   },
   typescript: {
     strict: false
