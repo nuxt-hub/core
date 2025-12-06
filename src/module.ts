2950efb2b327626cd2524dd24e3a24a9c272fb14
Sébastien Chopin <atinux@gmail.com>
2025-12-06 22:49:21 +0100

    remove no_bundle

--- src/module.ts
+++ src/module.ts
@@ -94,8 +94,7 @@ export default defineNuxtModule<ModuleOptions>({
       nuxt.options.nitro.cloudflare.nodeCompat = true
       nuxt.options.nitro.cloudflare.deployConfig = true
       nuxt.options.nitro.cloudflare.wrangler = defu(nuxt.options.nitro.cloudflare.wrangler, {
-        compatibility_flags: ['nodejs_compat_v2'],
-        no_bundle: true
+        compatibility_flags: ['nodejs_compat_v2']
       })
       // Remove trailing slash for prerender routes
       nuxt.options.nitro.prerender ||= {}
