e6f2d6859ca031f5aed2feb3c8926e43db0fc06b
Sébastien Chopin <atinux@gmail.com>
2025-12-06 13:37:40 +0100

    disable error throwing

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -276,9 +276,6 @@ export { db, schema }
 import * as schema from './db/schema.mjs'
 
 const binding = process.env.DB || globalThis.__env__?.DB || globalThis.DB
-if (!binding && !import.meta.prerender) {
-  throw new Error('DB binding not found')
-}
 const db = drizzle(binding, { schema })
 export { db, schema }
 `
@@ -345,9 +342,6 @@ export { db, schema }
 import * as schema from './db/schema.mjs'
 
 const hyperdrive = process.env.${bindingName} || globalThis.__env__?.${bindingName} || globalThis.${bindingName}
-if (!hyperdrive && !import.meta.prerender) {
-  throw new Error('${bindingName} binding not found')
-}
 const db = drizzle({ connection: hyperdrive.connectionString, schema })
 export { db, schema }
 `
