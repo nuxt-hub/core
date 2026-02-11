8af48979f7e28d3dc998cca68c5d2497ea625bbd
Sqh3rd <moitz417@gmail.com>
2026-02-11 16:49:46 +0100

    Fix copy pasta oversight

--- playground/server/db/schema/author.postgresql.ts
+++ playground/server/db/schema/author.postgresql.ts
@@ -1,7 +1,5 @@
 import { pgTable, text, serial } from 'drizzle-orm/pg-core'
 
-export { glasses } from '../glasses'
-
 export const author = pgTable('author', {
   id: serial().primaryKey(),
   name: text().notNull()
